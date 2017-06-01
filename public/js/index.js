f66a12ae2889751dd78b9a1be38ffe4d7d100410
ajith.manmadhan <ajith@surfline.com>
2017-06-01 14:57:11 -0700

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,14 @@ socket.on('connect', function () {
   console.log('Connected to server');
 });
 
+// socket.on('Welcome', function(message){
+//   console.log('Welcome', message);
+// });
+//
+// socket.on('NewUserJoined', function (message){
+//   console.log('NewUserJoined', message);
+// });
+
 socket.on('newMessage', function (message){
   console.log('Message received from server',message);
 });
--- server/server.js
+++ server/server.js
@@ -15,13 +15,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'New User joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (newMessage)=>{
     console.log('Message received from client', newMessage);
+
+    // socket.broadcast.emit('newMessage', {
+    //   from:newMessage.from,
+    //      text:newMessage.text,
+    //      createdAt: new Date().getTime()
+    // });
+
     io.emit('newMessage', {
       from:newMessage.from,
       text:newMessage.text,
       createdAt: new Date().getTime()
-    })
+    });
+
   });
 
   socket.on('disconnect', ()=>{

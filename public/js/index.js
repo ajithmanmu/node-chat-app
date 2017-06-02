3a8f7ddf7a26a65021aa3e1729e53e37b452d81a
ajith.manmadhan <ajith@surfline.com>
2017-06-02 14:27:06 -0700

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -24,20 +24,14 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-// socket.emit('createMessage', {
-//   from:'test@q.com',
-//   text:'Hi'
-// }, function (data){
-//   console.log('Got it', data);
-// });
-
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
+  var messageTextBox = jQuery('[name=message]');
   socket.emit('createMessage',{
     from:'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function (){
-
+    messageTextBox.val('');
   });
 });
 
@@ -46,12 +40,15 @@ locationButton.on('click', function (){
   if(!navigator.geolocation){
     return alert('Geolocation not supported');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function (position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function (){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
